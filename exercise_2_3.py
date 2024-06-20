fca7a9a18249e0f126797c5420028f0452e379a5
malvaro71 <55465735+malvaro71@users.noreply.github.com>
2024-06-20 16:39:04 +0200

    Create exercise_2_3.py

---
+++ exercise_2_3.py
@@ -0,0 +1,6 @@
+# A sailboat is moving with a speed of 4 km/h in the NNE (north-northeast) direction. 
+# What are the components of the speed of the ship in the North direction and in the East direction?
+
+# If we place the x-axis in the easterly direction and the y-axis in the north direction, the NNE 
+# (north-northeast) direction forms an angle of 67.5º (or 3PI/8 radians) with the east.
+# Then, x-component of speed of the ship is
