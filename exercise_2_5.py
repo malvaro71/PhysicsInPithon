1a0e3cae32826404d0f8fd2f0ee29803461bbf9b
malvaro71 <55465735+malvaro71@users.noreply.github.com>
2024-06-20 18:52:19 +0200

    Create exercise_2_5.py

---
+++ exercise_2_5.py
@@ -0,0 +1,18 @@
+# Find the scalar product of the vectors (5, −3, 2), (−2, 1, 3) and the angle they form. 
+
+# Import numpy
+import numpy as np
+
+# Input data
+a = np.array([5, -3, 2])
+b = np.array([-2, 1, 3])
+
+# Calculate the scalar product of vectors a and b
+scalar_product = numpy.dot(a, b)
+
+# Calculate the angle in radians formed by a and b
+angle = np.arccos(scalar_product/(np.linalg.norm(a)*np.linalg.norm(b)))
+
+# Print the results
+print('The scalar product of the vectors (5, −3, 2), (−2, 1, 3) is', scalar_product)
+print('the angle in radians formed by (5, −3, 2) and (−2, 1, 3) is', angle)
