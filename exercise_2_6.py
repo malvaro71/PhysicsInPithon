f478ac357bb5f66f870a12b7a946f8a9025b2774
malvaro71 <55465735+malvaro71@users.noreply.github.com>
2024-06-21 14:13:06 +0200

    Update and rename exercise_2_6 to exercise_2_6.py

---
+++ exercise_2_6.py
@@ -0,0 +1,13 @@
+# An airplane is moving with a speed of 600 km/h in the NE direction. 
+# Find the projection of its velocity onto the east direction.
+
+import math
+
+# Imput data
+airplane_speed = 600 # Km/h
+
+# Calculate the proyection using the scalar product
+speed_proy_E = airplane_speed * math.cos(math.pi / 4) # Angle from E to NE is PI/4 radians
+
+# Print the result
+print(f'The projection of airplane velocity')
